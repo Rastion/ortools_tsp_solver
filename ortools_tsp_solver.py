266fcc50b9065906ea2eff0d4ccfaddfb2c3d611
leonidas1312 <jonhkarystos@gmail.com>
2025-02-24 22:37:04 +0200

    Add/Update solver code & config

--- ortools_tsp_solver.py
+++ ortools_tsp_solver.py
@@ -37,7 +37,8 @@ class ORToolsTSPSolver(BaseOptimizer):
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = (
             routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-        
+        search_parameters.local_search_metaheuristic = (
+            routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
         search_parameters.time_limit.seconds = self.time_limit
 
         # Solve the problem.
